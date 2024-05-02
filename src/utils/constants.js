5755cb60aff4e50b850bacf038582e6ef0728ff7
Ashish Karn <82334486+akkarn1689@users.noreply.github.com>
2024-05-02 15:45:35 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "";
+const GOOGLE_API_KEY = "AIzaSyDjDSTxx_jSBpxgfhEdF8T_CXxiIiC30UE";
 
 export const YOUTUBE_VIDEO_API = 
         "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key="+
